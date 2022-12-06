92a7c1ffecf914d57dff50b82d50ea2b8fbea44a
akyua <gabrielnovatoo@gmail.com>
2022-12-06 17:44:12 -0300

    Created the beginning of the webpage/model

---
+++ main.js
@@ -0,0 +1,12 @@
+let click = true
+
+function seconds(){
+    if (click = true){
+        for(let timer = 1; timer <= 60; timer++){
+            console.log(timer);
+        }
+    }
+}
+
+console.log(seconds());
+
