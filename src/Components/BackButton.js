d12f65b4981141f9c4db6b06c69bf2f0b69328dd
Gabriel <gabrielnovatoo@gmail.com>
2023-02-24 18:29:36 -0300

    Fix bug

--- src/Components/BackButton.js
+++ src/Components/BackButton.js
@@ -1,4 +1,4 @@
-const BackButton = (props) => {
+function BackButton(props){
     return(
         <button {...props} className={'with-text'}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
--- src/Components/PlayButton.js
+++ src/Components/PlayButton.js
@@ -1,4 +1,4 @@
-const PlayButton = (props) => {
+function PlayButton (props){
     return(
         <button {...props}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -7,7 +7,7 @@ const PlayButton = (props) => {
             </svg>
         </button>
 
-    )
+    );
 }
 
 export default PlayButton;
